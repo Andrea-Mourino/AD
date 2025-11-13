19854384b233471828b0fd721999ce776e1ad28f
Andrea <amourinodesousa@danielcastelao.com>
2025-11-13 20:50:36 +0100

    modificaciones

--- PracticaPokemon/src/Main.java
+++ PracticaPokemon/src/Main.java
@@ -14,8 +14,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-
-
         PokedexService.insertPokedex(new Pokedex("Pikachu", 10, "Es adorable"));
         PokedexService.insertPokedex(new Pokedex("Charizar", 10, "Es adorable"));
         PokedexService.insertPokedex(new Pokedex("Eeevee", 10, "Es una bolita de pelo"));
@@ -28,20 +26,24 @@ public class Main {
         PokedexService.insertPokedex(new Pokedex("Charmander", 9.7, "Es peak"));
 
 
+        //Esto es para DATE (de la variable Adestrador)
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-//            AdestradorService.insertAdestrador(new Adestrador("Figueroa", sdf.parse("10/02/2002")));
-//            AdestradorService.insertAdestrador(new Adestrador("Andrea", sdf.parse("31/05/2004")));
+                AdestradorService.insertAdestrador(new Adestrador("Figueroa", sdf.parse("10/02/2002")));
+                AdestradorService.insertAdestrador(new Adestrador("Andrea", sdf.parse("31/05/2004")));
 
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
+
+        //La funcion exige que al llamaar a la funcion sea un List
         List<Pokedex> listado = PokedexService.leerPokedex();
-        List<Adestrador> listado2 = AdestradorService.leerAdestrador();
+        listado.forEach(System.out::println); //esto se pone una vez para mostrarlo
 
 
-        Pokedex P1 = listado.get(2);
+
+        Pokedex P1 = listado.get(2); //esto es para coger algo de la lista
         Pokedex P2 = listado.get(9);
 
 
@@ -49,11 +51,22 @@ public class Main {
         PokedexService.actualizarPokedexNome(P1, "Silveon");
         PokedexService.actualizarPokedexNome(P2, "Charmileon");
 
-        listado = PokedexService.leerPokedex();
-        listado.forEach(System.out::println);
+
+        listado = PokedexService.leerPokedex(); //esto es para llamar a listado despues de actualizar la lista
+        listado.forEach(System.out::println); //esto para mostrarlo de nuevo despues de actualizarlo
 
 
-        Adestrador A1 = listado2.get(0);
+        PokedexService.eliminarPokedexTodos();
+
+
+        //-----------------------------------------------
+
+        //La funcion exige que al llamaar a la funcion sea un List
+        List<Adestrador> listado2 = AdestradorService.leerAdestrador();
+        listado.forEach(System.out::println); //esto se pone una vez para mostrarlo
+
+
+        Adestrador A1 = listado2.get(0); //esto es para coger algo de la lista
         Adestrador A2 = listado2.get(1);
 
         //Esto es para actualizar un Adestrador(en este caso el nombre)
@@ -61,13 +74,16 @@ public class Main {
         AdestradorService.actualizarAdestradorNome(A2, "Jotaro");
 
         listado2 = AdestradorService.leerAdestrador();
-        listado2.forEach(System.out::println);
+        listado2.forEach(System.out::println);  //esto para mostrarlo de nuevo despues de actualizarlo
 
 
-        PokedexService.eliminarPokedexTodos();
         AdestradorService.eliminarAdestradorTodos();
 
 
+
+        //-----------------------------------------------
+
+        //esto es para el XML NO HACE FALTA
         try {
             PokedexXML.escribirPokedex(listado);
             AdestradoresXML.escribirAdestradores(listado2);
